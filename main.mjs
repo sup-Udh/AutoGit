e1b4e1e935ce73e1f661b880d11bcc3c7a5f8df9
Udhay <57910021+code123841@users.noreply.github.com>
2021-12-22 08:47:31 +0530

    first commit

---
+++ main.mjs
@@ -0,0 +1,27 @@
+import prompts  from "prompts";
+const questions = [
+  {
+    type: 'text',
+    name: 'username',
+    message: 'What is your GitHub username?'
+  },
+  {
+    type: 'number',
+    name: 'age',
+    message: 'How old are you?'
+  },
+  {
+    type: 'text',
+    name: 'about',
+    message: 'Tell something about yourself',
+    initial: 'Why should I?'
+  }
+];
+
+(async () => {
+  const response = await prompts(questions);
+
+  // => response => { username, age, about }
+  // Getting the stuff from the user input
+  console.log(response.username);
+})();
